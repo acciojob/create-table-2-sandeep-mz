232a7ac8fb32f7e395f745538e47809cba36b9f9
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-09-30 20:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function createTable() {
-    //Write your code here
-  
-}
+ function createTable() {
+	 // write your code here
+            const rows = prompt("Input number of rows:");
+            const columns = prompt("Input number of columns:");
+            
+            const table = document.getElementById("myTable");
+            table.innerHTML = ""; // Clear existing table content if any
+            
+            for (let i = 0; i < rows; i++) {
+                const row = table.insertRow(i);
+                for (let j = 0; j < columns; j++) {
+                    const cell = row.insertCell(j);
+                    cell.textContent = `Row-${i} Column-${j}`;
+                }
+            }
+        }
